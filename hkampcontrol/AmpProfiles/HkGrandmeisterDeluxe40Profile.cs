12efe241f6f4f632d95e79cdb6966c1e0326ad21
Nicholas Ruffing <nicholasruffing70@gmail.com>
2019-07-30 21:53:39 -0400

    Create start of amp profile

---
+++ hkampcontrol/AmpProfiles/HkGrandmeisterDeluxe40Profile.cs
@@ -0,0 +1,17 @@
+﻿namespace hkampcontrol.AmpProfiles
+{
+    /// <summary>
+    /// Implementation that defines an amplifier profile for the Hughes & Kettner GrandMeister Deluxe 40
+    /// containing the controller number for each possible control change event.
+    /// http://hughes-and-kettner.com/products/grandmeister/grandmeister-deluxe-40/
+    /// </summary>
+    public class HkGrandmeisterDeluxe40Profile : IAmpProfile
+    {
+        /// <summary>
+        /// Gets the control number for toggling the boost. It is assumed that sending a value
+        /// of 0-63 will turn the boost off and 64-127 will turn it on. For any value over 127
+        /// the higher nibble will be ignored.
+        /// </summary>
+        public byte Boost => 64;
+    }
+}
---
+++ hkampcontrol/AmpProfiles/IAmpProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace hkampcontrol.AmpProfiles
+{
+    /// <summary>
+    /// Interface that defines an amplifier profile containing the controller number for each possible 
+    /// control change event.
+    /// </summary>
+    public interface IAmpProfile
+    {
+        /// <summary>
+        /// Gets the control number for toggling the boost. It is assumed that sending a value
+        /// of 0-63 will turn the boost off and 64-127 will turn it on. For any value over 127
+        /// the higher nibble will be ignored.
+        /// </summary>
+        byte Boost { get; }
+    }
+}
