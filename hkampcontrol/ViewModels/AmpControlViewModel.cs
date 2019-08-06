55602238aecb112d745a72834cbc8e9b2d2daf84
Nicholas Ruffing <nicholasruffing70@gmail.com>
2019-08-05 20:54:37 -0400

    Add midi output device selection

--- hkampcontrol/ViewModels/AmpControlViewModel.cs
+++ hkampcontrol/ViewModels/AmpControlViewModel.cs
@@ -1,4 +1,5 @@
-﻿using hkampcontrol.AmpProfiles;
+﻿using FluentMidi;
+using hkampcontrol.AmpProfiles;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -7,15 +8,21 @@ namespace hkampcontrol.ViewModels
     public sealed class AmpControlViewModel : ViewModelBase
     {
         private IAmpProfile _selectedProfile;
+        private IMidiOutputDevice _selectedDevice;
 
         public AmpControlViewModel()
             : base()
         {
             this.Profiles = new ObservableCollection<IAmpProfile>(AmpProfiles.AmpProfiles.All);
-            this.SelectedProfile = Profiles.FirstOrDefault();
+            this.SelectedProfile = this.Profiles.FirstOrDefault();
+
+            this.Devices = new ObservableCollection<IMidiOutputDevice>(MidiDeviceLocator.GetAllOutputDevices());
+            this.SelectedDevice = this.Devices.FirstOrDefault();
         }
 
         public ObservableCollection<IAmpProfile> Profiles { get; }
+
+        public ObservableCollection<IMidiOutputDevice> Devices { get; }
 
         public IAmpProfile SelectedProfile
         {
@@ -25,7 +32,20 @@ namespace hkampcontrol.ViewModels
                 if (this._selectedProfile != value)
                 {
                     this._selectedProfile = value;
-                    OnPropertyChanged(nameof(SelectedProfile));
+                    this.OnPropertyChanged(nameof(SelectedProfile));
+                }
+            }
+        }
+
+        public IMidiOutputDevice SelectedDevice
+        {
+            get => this._selectedDevice;
+            set
+            {
+                if (this._selectedDevice != value)
+                {
+                    this._selectedDevice = value;
+                    this.OnPropertyChanged(nameof(SelectedDevice));
                 }
             }
         }
